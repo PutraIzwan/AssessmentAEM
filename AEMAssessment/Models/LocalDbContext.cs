5070e4f994d8e370b36b9ac484a81968de611669
Putra <putra96izwan@gmail.com>
2023-08-28 22:47:00 +0800

    Add project files.

---
+++ AEMAssessment/Models/LocalDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AEMAssessment.Models
+{
+    public partial class LocalDbContext : DbContext
+    {
+        public LocalDbContext()
+        {
+                
+        }
+
+        public LocalDbContext(DbContextOptions<LocalDbContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Platform> Platform { get; set; }
+        public virtual DbSet<Well> Well { get; set; }
+    }
+}
